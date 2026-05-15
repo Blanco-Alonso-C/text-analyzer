b6d19fe75cbbff8df613aa3860f89530e1b81598
Carme Xiu Blanco Alonso <blanco.alonso.c@outlook.com>
2026-05-15 19:11:04 +0200

    Delete text-analyzer directory

--- text-analyzer/data/dictionaries.py
+++
@@ -1,9 +0,0 @@
-# Now this is the core of the program mainly
-dictionary_alpha = {}
-dictionary_alpha_symbol = {}
-dictionary_numbers = {}
-dictionary_nouns = {}
-dictionary_words = {}
-dictionary_sentence = {}
-dictionary_paragraph = {}
-dictionary_symbols = {}
